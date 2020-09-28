3c11987a6de305063e81640a2e92025596102da8
knyeol <knyeol@outlook.com>
2020-09-28 16:19:00 -0700

    added files

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4d55f9e8cbea44142a08d2ab28e87fac'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'afa05115-0a5c-4b2b-a498-0fce224518eb'
+};
