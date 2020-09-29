575c78954c6151b98a33c59ed4bfc2e0044d6646
knyeol <knyeol@outlook.com>
2020-09-29 16:30:31 -0700

    updated css

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,18 +1,24 @@
 import React from "react";
+import { FaCameraRetro } from "react-icons/fa";
 
 function Navbar() {
   return (
     <navbar-container>
       <navbar-content>
-        <a href="/" className="navbar-item">
-          Home
+        <a href="/" className="navbar-home">
+          <FaCameraRetro className="navbar-icon" />
         </a>
-        <a href="/portfolio" className="navbar-item">
-          Works
-        </a>
-        <a href="/about" className="navbar-item">
-          About
-        </a>
+        <navbar-items>
+          <a href="/" className="navbar-item">
+            Home
+          </a>
+          <a href="/portfolio" className="navbar-item">
+            Works
+          </a>
+          <a href="/about" className="navbar-item">
+            About
+          </a>
+        </navbar-items>
       </navbar-content>
     </navbar-container>
   );
