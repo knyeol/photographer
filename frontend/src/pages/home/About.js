f6f5ec75c1aa20eb321f45dbcd9ebcc95ab256a9
knyeol <knyeol@outlook.com>
2020-10-08 16:22:21 -0700

    updated modal and css

--- frontend/src/pages/home/About.js
+++ frontend/src/pages/home/About.js
@@ -5,12 +5,14 @@ function About() {
     <content-container>
       <text-subtitle>About</text-subtitle>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero autem
-        cupiditate atque neque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum ipsum
+        iusto pariatur, mollitia in quidem, alias ex porro cumque illum ducimus.
       </p>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae,
-        repellat ea?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quidem
+        nihil non, ex pariatur minus tempora inventore provident! Ipsum ullam
+        facere et beatae eligendi, expedita culpa sunt, mollitia, magni
+        repudiandae unde.
       </p>
       <a href="/about" className="link">
         Read More
